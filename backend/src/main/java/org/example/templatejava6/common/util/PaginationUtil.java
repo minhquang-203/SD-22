e6bb8d02f01083b1779b6f6c5195242c0b6f781c
minhquang-203 <minhquang111203@gmail.com>
2026-06-12 21:33:50 +0700

    Tạo util phaan trang

--- backend/src/main/java/org/example/templatejava6/common/util/PaginationUtil.java
+++ backend/src/main/java/org/example/templatejava6/common/util/PaginationUtil.java
@@ -1,4 +1,23 @@
 package org.example.templatejava6.common.util;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
 public class PaginationUtil {
+
+    public static Pageable create(int page, int size) {
+        return PageRequest.of(Math.max(page - 1, 0), size);
+    }
+
+    public static Pageable create(int page, int size, String sortBy, boolean desc) {
+        int pageIndex = Math.max(page - 1, 0);
+        Sort sort = desc ? Sort.by(sortBy).descending() : Sort.by(sortBy).ascending();
+        return PageRequest.of(pageIndex, size, sort);
+    }
+
+    // co sap xep
+    public static Pageable create(int page, int size, Sort sort) {
+        return PageRequest.of(Math.max(page - 1, 0), size, sort);
+    }
 }
